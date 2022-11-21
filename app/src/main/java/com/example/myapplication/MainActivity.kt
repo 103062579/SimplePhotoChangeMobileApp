c58bafae558d4235a21dcd7afa5b5e612f58069f
Darcy Giblin <darcy.giblin@asta.com.au>
2022-11-22 09:46:05 +1100

    updated icons, switches between on click

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    var state = R.drawable.check_circle_outline_black_24dp
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val image = findViewById<ImageView>(R.id.imageView)
+        image.setOnClickListener{
+            state = when (state){
+                R.drawable.check_circle_outline_black_24dp -> R.drawable.done_black_24dp
+                R.drawable.done_black_24dp -> R.drawable.trending_up_black_24dp
+                else->R.drawable.check_circle_outline_black_24dp
+            }
+            image.setImageDrawable(getDrawable(state))
+        }
     }
 }
